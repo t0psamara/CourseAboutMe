286e226fd16c6a94f38300da93c2972cb54c441b
Nikita Petrenkov <nikita.petrenkov@rtlabs.ru>
2020-11-26 21:15:54 +0400

    Разработана верстак, разработан функционал удаления и получения данных.

--- CoureAboutMe/ViewController.swift
+++ CoureAboutMe/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
+
+struct MyData {
+    
+    let fio: String = "Никита Петренков"
+    
+    let foto: UIImage? = UIImage(named: "Logo")
+    
+    let aboutMeInfo: String = "Я являюсь разработчиком мобильных приложений на платформе iOS в одной из it компаний России"
+}
+
+
+
+
 class ViewController: UIViewController {
 
+    /// Поле с ФИО
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    /// Аватар
+    @IBOutlet weak var fotoView: UIImageView!
+    
+    /// Поле с информацией о себе
+    @IBOutlet weak var aboutMeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonPressed() {
+        
+        let data = MyData()
+        
+        nameLabel.text = data.fio
+        
+        fotoView.image = data.foto
+        
+        aboutMeLabel.text = data.aboutMeInfo
+        
+    }
+    
+    @IBAction func deleteData() {
+        nameLabel.text = nil
+        
+        fotoView.image = nil
+        
+        aboutMeLabel.text = nil
+        
+    }
 }
 
